ab174b766cbbbadd8bccc21ce99c20417d304e15
dheepan23 <dheepan2311@gmail.com>
2026-02-10 16:29:41 +0530

    Implemented the crud operations for bookings

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
 from database.database import engine,Base
-from app.routers.users import router
-from database.models import Users
+from app.routers import users,bookings,services
 app = FastAPI()
 
 Base.metadata.create_all(bind=engine)
-app.include_router(router)
+app.include_router(users.router)
+app.include_router(bookings.router)
+app.include_router(services.router)
