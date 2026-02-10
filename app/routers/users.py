7b5a1d677da514f2a04e02f0a504da532225a166
dheepan23 <dheepan2311@gmail.com>
2026-02-10 16:31:51 +0530

    created a API for retrieving all the users

--- app/routers/users.py
+++ app/routers/users.py
@@ -3,7 +3,8 @@ from sqlalchemy.orm import Session
 from database.database import get_db
 from app.schemas.user_schemas import UserCreate,UserResponse,UserUpdate
 from app.services.user_service import create_user,get_user_by_id,delete_user_by_id,update_user_by_id
-
+from database.models import Users
+from fastapi.responses import JSONResponse
 router = APIRouter(prefix="/users",tags=["Users"])
 
 @router.post("/create_user",response_model=UserCreate)
@@ -16,14 +17,28 @@ def create_users(user: UserCreate, db:Session=Depends(get_db)):
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
             detail="An error occurred while creating the user",
         )
+@router.get("/get_users")
+def get_users(db:Session = Depends(get_db)):
+    try:
+        users = db.query(Users).filter(Users.is_active.is_(True))
+        result =[{"id":user.id,"name":user.name,"role":user.role.value}for user in users]
+        return JSONResponse(
+            status_code=status.HTTP_200_OK,
+            content=result,
+        )
+    except HTTPException as e:
+        raise e
+    except Exception:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="An error occurred while retrieving the users",
+        )
 
 @router.get("/get_user",response_model=UserResponse)
 def get_user(user_id:int,db:Session=Depends(get_db)):
     try:
         return get_user_by_id(db,user_id)
     except HTTPException:
-        import traceback
-        traceback.print_exc()
         raise HTTPException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
             detail="An error occurred while retrieving the user",
