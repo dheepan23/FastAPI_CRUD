3c0545cf64349f0ff74b2d1021b58194e492a4d7
dheepan23 <dheepan2311@gmail.com>
2026-02-26 11:59:44 +0530

    service_crud - created the get api for services

--- app/services/service_service.py
+++ app/services/service_service.py
@@ -3,7 +3,7 @@ from app.schemas.service_schemas import ServiceCreate,ServiceResponse
 from sqlalchemy.orm import Session
 from database.models import Services
 from fastapi.responses import JSONResponse
-from fastapi import status
+from fastapi import status,HTTPException
 
 def create_service(db:Session,service:ServiceCreate):
     new_service = Services(
@@ -17,4 +17,24 @@ def create_service(db:Session,service:ServiceCreate):
     return JSONResponse(
         status_code=status.HTTP_201_CREATED,
         content="Service created successfully",
+    )
+
+def get_services(db:Session,service_id):
+    service = db.query(Services).filter(Services.id ==service_id).first()
+    if not service:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Service not found",
+        )
+    return JSONResponse(
+        status_code=status.HTTP_200_OK,
+        content={
+            "message": "Service retrieved successfully",
+            "data":{
+                "service_id" :  service.id,
+                "name" : service.name,
+                "is_active" : service.is_active,
+                "description" : service.description,
+                },
+        }
     )
